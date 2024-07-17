caa033b01cef9e85fe0245e12fa22fe79d54cbd1
SaiVikrantG <saivikrant.cs21@bmsce.ac.in>
2024-07-17 14:06:23 +0530

    Committing match game.

---
+++ matchGame/src/components/Game/index.js
@@ -0,0 +1,52 @@
+import './index.css'
+import TabItem from '../TabItem'
+import ThumbnailItem from '../ThumbnailItem'
+
+const Game = props => {
+  const {
+    allImagesList,
+    tabs,
+    thumbnailImages,
+    activeTabId,
+    currentImageId,
+    onClickTab,
+    onClickThumbnail,
+  } = props
+
+  const getImage = () =>
+    allImagesList.find(image => image.id === currentImageId)
+  const renderTabs = () => (
+    <ul className="tab-items-container">
+      {tabs.map(tab => (
+        <TabItem
+          tabDetails={tab}
+          active={tab.tabId === activeTabId}
+          onClick={onClickTab}
+          key={tab.tabId}
+        />
+      ))}
+    </ul>
+  )
+  const renderThumbnails = () => (
+    <ul className="thumbnails-container">
+      {thumbnailImages.map(thumbnail => (
+        <ThumbnailItem
+          thumbnail={thumbnail}
+          key={thumbnail.id}
+          onClickThumbnail={onClickThumbnail}
+        />
+      ))}
+    </ul>
+  )
+
+  const {imageUrl} = getImage()
+  return (
+    <div className="game-container">
+      <img className="match-image" src={imageUrl} alt="match" />
+      {renderTabs()}
+      {renderThumbnails()}
+    </div>
+  )
+}
+
+export default Game
