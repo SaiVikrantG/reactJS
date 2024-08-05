2cebcf57fdeaa12b905e4e2b13c0aa312b2ea066
SaiVikrantG <saivikrant.cs21@bmsce.ac.in>
2024-08-05 22:02:12 +0530

    Committing projectsShowcase files with bash script.

---
+++ projectsShowcase/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-container">
+    <div className="header-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
+        alt="website logo"
+        className="website-logo"
+      />
+    </div>
+  </nav>
+)
+
+export default Header
